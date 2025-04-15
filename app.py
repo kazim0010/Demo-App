4b7999e1331c0273c6bce614cbffb4360357ebf3
Kazim <kazhas88412@gmail.com>
2025-04-15 09:19:27 +0500

    Add robots.txt

--- app.py
+++ app.py
@@ -6,5 +6,9 @@ app = Flask(__name__)
 def index():
     return render_template("index.html")
 
+@app.route('/robots.txt')
+def robots():
+    return render_template("robots.html")
+
 if __name__ == "__main__":
     app.run(debug=True)
